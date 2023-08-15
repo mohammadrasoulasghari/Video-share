0b293c8c6fb1077ede03d2afeab21f41850bc1b4
mohammadRasoul <mohammadrasoulasghari1@gmail.com>
2023-08-15 20:53:57 +0330

    install Laravel Breez

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
 import './bootstrap';
-import "../css/style.css"
-import "../css/responsive.css"
-import "../css/bootstrap.min.css"
-import "./jquery-3.2.1.min"
-import "./jquery.sticky-kit.min"
-import "./custom"
-import "./bootstrap.min"
-import "./imagesloaded.pkgd.min"
-import "./grid-blog.min"
-import "./imagesloaded.pkgd.min"
+
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\VideoController;
-use App\Http\Controllers\CategoryVideoController;
-use App\Models\Category;
-use Illuminate\Database\Query\IndexHint;
-use App\Models\Video;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,18 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[IndexController::class,'index'])->name('index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/videos/create',[VideoController::class,'create']);
-Route::post('/videos',[VideoController::class,'store'])->name('videos.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//route Model Bounding
-Route::get('/videos/{video}',[VideoController::class,'show'])->name('videos.show');
-Route::get('/videos/{video}/edit',[VideoController::class,'edit'])->name('videos.edit');
-Route::post('/videos/{video}',[VideoController::class,'update'])->name('videos.update');
-
-Route::get('/categories/{category:slug}/videos',[CategoryVideoController::class,'index'])->name('categories.videos.index');
-
-
-
+require __DIR__.'/auth.php';
