6e1d9283e1af7cd542ffd243f8c427392b7bb5e0
mohammadRasoul <mohammadrasoulasghari1@gmail.com>
2023-09-01 11:29:07 +0330

    The end of the like section

--- app/Models/Traits/Likeable.php
+++ app/Models/Traits/Likeable.php
@@ -2,6 +2,7 @@
 namespace App\Models\Traits;
 
 use App\Models\Like;
+use App\Models\User;
 
 trait Likeable{
 
@@ -16,5 +17,30 @@ trait Likeable{
     public function getDisLikeCountAttribute()
     {
         return $this->likes()->where('vote',1)->count();
+    }
+    public function likedBy(User $user)
+    {
+        if ($this->isLikedBy($user)) return;
+        return $this->likes()->create([
+            'vote' => 1,
+            'user_id' => $user->id
+        ]);
+    }
+    public function DislikedBy(User $user)
+    {
+        if ($this->isDisLikedBy($user)) return;
+
+        return $this->likes()->create([
+            'vote' => -1,
+            'user_id' => $user->id
+        ]);
+    }
+    public function isLikedBy(User $user)
+    {
+        return $this->likes()->where('vote',1)->where('user_id',$user->id)->exists();
+    }
+    public function isDisLikedBy(User $user)
+    {
+        return $this->likes()->where('vote',-1)->where('user_id',$user->id)->exists(); 
     }
 }
---
+++ app/Notifications/ResourceWasLiked.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResourceWasLiked extends Notification
+{
+    use Queueable;
+    private $likeable;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($likeable)
+    {
+        $this->likeable = $likeable;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('your video was liked');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
